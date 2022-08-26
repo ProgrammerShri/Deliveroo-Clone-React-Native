3655bde3c7d44a38fbe6fd6fe61dbcf36035645f
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-08-26 19:15:31 +0530

    feat : Global Basket UI & functionality is added

---
+++ components/BasketIcon.js
@@ -0,0 +1,35 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import tw from "twrnc";
+import { selectBasketItems, selectBasketTotal } from "../features/basketSlice";
+import { useNavigation } from "@react-navigation/native";
+import Currency from "react-currency-formatter";
+
+const BasketIcon = () => {
+  const items = useSelector(selectBasketItems);
+  const navigation = useNavigation();
+  const basketTotal = useSelector(selectBasketTotal);
+  return (
+    <View style={tw`absolute bottom-10 w-full z-50`}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Basket")}
+        style={tw`bg-[#00CCBB] mx-5 p-4 rounded-lg flex-row  items-center justify-between`}
+      >
+        <Text
+          style={tw`text-white font-extrabold text-lg bg-[#01A296] py-1 px-2`}
+        >
+          {items.length}{" "}
+        </Text>
+        <Text style={tw`flex-1 text-white font-extrabold text-lg text-center`}>
+          View Basket
+        </Text>
+        <Text style={tw`text-lg text-white font-extrabold`}>
+          <Currency quantity={basketTotal} currency="INR" />
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BasketIcon;
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -5,6 +5,7 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
 import Icon from "../components/Icon";
 import DishRow from "../components/DishRow";
+import BasketIcon from "../components/BasketIcon";
 
 const RestaurantScreen = () => {
   const { params } = useRoute();
@@ -29,84 +30,87 @@ const RestaurantScreen = () => {
   }, []);
 
   return (
-    <ScrollView>
-      <View style={tw`relative`}>
-        <Image
-          source={{ uri: urlFor(imgUrl).url() }}
-          style={tw`h-56 w-full bg-gray-300 p-4`}
-        />
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          style={tw`absolute top-14 left-5 p-4 bg-gray-100 rounded-full shadow z-10`}
-        >
-          <Icon type="ant" name="arrowleft" size={20} color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
+    <>
+      <BasketIcon />
 
-      <View style={tw`bg-white`}>
-        <View style={tw`px-4 pt-4`}>
-          <Text style={tw`text-3xl font-bold`}>{title} </Text>
-          <View style={tw`flex-row items-center my-1 `}>
-            <Icon
-              type="ant"
-              name="star"
-              size={20}
-              color="green"
-              style={tw`opacity-50`}
-            />
-            <Text style={tw`text-gray-500 text-xs pl-1`}>
-              <Text style={tw`text-green-500`}>{rating}</Text> · {genre}
-            </Text>
+      <ScrollView>
+        <View style={tw`relative`}>
+          <Image
+            source={{ uri: urlFor(imgUrl).url() }}
+            style={tw`h-56 w-full bg-gray-300 p-4`}
+          />
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={tw`absolute top-14 left-5 p-4 bg-gray-100 rounded-full shadow z-10`}
+          >
+            <Icon type="ant" name="arrowleft" size={20} color="#00CCBB" />
+          </TouchableOpacity>
+        </View>
 
-            <Icon
-              type="evil"
-              name="location"
-              size={24}
-              color="gray"
-              style={tw`opacity-50`}
-            />
-            <Text style={tw`text-gray-500 text-xs pl-1`}>
-              Nearby · {address}
+        <View style={tw`bg-white`}>
+          <View style={tw`px-4 pt-4`}>
+            <Text style={tw`text-3xl font-bold`}>{title} </Text>
+            <View style={tw`flex-row items-center my-1 `}>
+              <Icon
+                type="ant"
+                name="star"
+                size={20}
+                color="green"
+                style={tw`opacity-50`}
+              />
+              <Text style={tw`text-gray-500 text-xs pl-1`}>
+                <Text style={tw`text-green-500`}>{rating}</Text> · {genre}
+              </Text>
+
+              <Icon
+                type="evil"
+                name="location"
+                size={24}
+                color="gray"
+                style={tw`opacity-50`}
+              />
+              <Text style={tw`text-gray-500 text-xs pl-1`}>
+                Nearby · {address}
+              </Text>
+            </View>
+
+            <Text style={tw`text-gray-500 text-sm mt-2 pb-4`}>
+              {short_description}
             </Text>
           </View>
 
-          <Text style={tw`text-gray-500 text-sm mt-2 pb-4`}>
-            {short_description}
-          </Text>
+          <TouchableOpacity
+            style={tw`flex-row items-center p-4 border-t border-gray-300 justify-between`}
+          >
+            <Icon
+              type="ant"
+              name="questioncircleo"
+              size={20}
+              color="black"
+              style={tw`opacity-50`}
+            />
+            <Text style={tw`text-sm font-bold -ml-36`}>
+              Have a food allergy ?
+            </Text>
+            <Icon type="ant" name="right" size={20} color="#00CCBB" />
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity
-          style={tw`flex-row items-center p-4 border-t border-gray-300 justify-between`}
-        >
-          <Icon
-            type="ant"
-            name="questioncircleo"
-            size={20}
-            color="black"
-            style={tw`opacity-50`}
-          />
-          <Text style={tw`text-sm font-bold -ml-36`}>
-            Have a food allergy ?
-          </Text>
-          <Icon type="ant" name="right" size={20} color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
-
-      <View>
-        <Text style={tw`px-4 pt-6 mb-3 font-bold text-xl`}>Menu</Text>
-
-        {dishes.map((dish) => (
-          <DishRow
-            key={dish._id}
-            id={dish._id}
-            name={dish.name}
-            description={dish.short_description}
-            price={dish.price}
-            image={dish.image}
-          />
-        ))}
-      </View>
-    </ScrollView>
+        <View style={tw`pb-36`}>
+          <Text style={tw`px-4 pt-6 mb-3 font-bold text-xl`}>Menu</Text>
+          {dishes.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
